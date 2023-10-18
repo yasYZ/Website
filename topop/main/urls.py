009423e34aaa58c4a43f88ff7b165a03fc065914
yasYZ <yasinyazdani71@gmail.com>
2023-10-18 19:46:30 +0330

    backend v1.0.0.52

--- topop/main/urls.py
+++ topop/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import run_index, Login_Register
+from .views import run_index, Login_Register, run_codes_page, news, contactus
 
 urlpatterns = [
-    path('Home/', run_index),
-    path('LoginSignup/', Login_Register)
-
-
+    path('Home/', run_index, name="Home"),
+    path('LoginSignup/', Login_Register, name="LoginSignup"),
+    path('Shop/', run_codes_page, name="codes"),
+    path('News/', news, name="news"),
+    path('Contactus/', contactus, name="Contactus"),
 ]
