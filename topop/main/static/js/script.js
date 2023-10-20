df954a24abf48eaae9cd51f4418ab45b2c85e693
IMRaxerr <radinraxer@gmail.com>
2023-10-20 19:33:37 +0330

    backend v1.0.0.52

--- topop/main/static/js/script.js
+++ topop/main/static/js/script.js
@@ -10,4 +10,18 @@ registerlink.addEventListener('click', ()=>{
 loginlink.addEventListener('click', ()=>{
     wrapper.classList.remove('active')
 })
+
 //Login / Sign_up End
+
+//reset password
+const wrapper2 = document.querySelector('.wrapper');
+const loginlink3 = document.querySelector('.login-link');
+const resetpassword =document.querySelector('.reset-link')
+
+resetpassword.addEventListener('click', ()=>{
+    wrapper2.classList.add('active2')
+})
+
+loginlink3.addEventListener('click', ()=>{
+    wrapper2.classList.remove('active2')
+})
